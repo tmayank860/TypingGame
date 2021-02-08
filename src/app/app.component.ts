181436ff75d1f91639f0c4821572e157a7ca3ac1
tmayank860 <tmayank860@gmail.com>
2021-02-08 21:27:45 +0530

    TypingGame

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild } from '@angular/core';
+import {lorem} from 'faker'
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TypingGame';
+  randomText=lorem.sentence();
+  enteredText="";
+  @ViewChild('text') inputName;
+
+  onInput(value:string){
+    this.enteredText=value;
+  }
+  compareCharacter(randomChar:string,enteredChar:string){
+    if(!enteredChar){
+      return 'pending';
+    }else if(randomChar===enteredChar){
+      return 'matching';
+    }else{
+      return 'different'
+    }
+  }
+  tryAgain(){
+    this.randomText=lorem.sentence();
+    this.enteredText="";
+    this.inputName.nativeElement.value = '';
+    
+    
+  }
 }
